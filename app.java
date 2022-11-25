190ab1b7e72d195b86254d1df3ff06d0a203bcfb
Cloud User <ec2-user@ip-172-31-95-33.ec2.internal>
2022-11-25 00:04:05 +0000

    first paypal credit card

---
+++ app.java
@@ -0,0 +1,8 @@
+#!/bin/bash/ 
+This application is for developers 
+Tell them not to push on the master branch 
+The can push on the dev branch 
+They must know how to collaborate 
+The environment is configured and secured 
+Now they can push to github 
+Author: Jennifer
